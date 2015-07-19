6d1896ba24bcf011782df6277b5a7b1e9c4084e7
Melvin Carvalho <melvincarvalho@gmail.com>
2015-07-19 02:08:56 +0200

    setup

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+#
+#          This script uses sets an initial coin offering for
+#          an initially setup sqlite3 database
+#
+# ------------------------------------------------------------------
+
+# eg setup.sh "https://localhost/etc/wallet/github.com/linkeddata/SoLiD/wallet" "https://deiu.me/profile#me"
+#
+WALLET="$1"
+FOUNDER="$2"
+
+rm credit.db
+
+./createsqlite.sh
+./ico.sh
+./founder.sh "$FOUNDER"
+
+nodejs ./commits.js
+nodejs ./issues.js
+nodejs ./comments.js
